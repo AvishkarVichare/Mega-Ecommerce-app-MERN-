1155fd960e890a046803ddba0c450718cfa5402e
Avishkar Vichare <avie9500@gmail.com>
2022-12-30 15:59:35 +0530

    configured morgan cors and cookie parser

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+// morgan logger 
 app.use(morgan('tiny')); // combined can be used
 
 export default app;
